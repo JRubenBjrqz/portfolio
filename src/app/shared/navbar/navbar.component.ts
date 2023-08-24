b54caa2bd8d2ebc8d1a53460f6da9e9d9e86038e
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-08-23 18:38:23 -0700

    Dynamic navbar implemented

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'navbar',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() section: string;
 
   constructor() { }
 
