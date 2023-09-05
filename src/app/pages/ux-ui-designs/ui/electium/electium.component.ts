6baf0978a39682a51c6467aa244c204ae12eb436
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-09-05 03:37:34 -0700

    Favicon and titles

--- src/app/pages/ux-ui-designs/ui/electium/electium.component.ts
+++ src/app/pages/ux-ui-designs/ui/electium/electium.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-electium',
   templateUrl: './electium.component.html',
-  styleUrls: ['./electium.component.scss']
+  styleUrls: ['./electium.component.scss'],
 })
 export class ElectiumComponent implements OnInit {
-
-  constructor() { }
+  constructor(private titleService: Title) {}
 
   ngOnInit(): void {
+    this.titleService.setTitle('Electium - Rubén Bojórquez');
   }
-
 }
